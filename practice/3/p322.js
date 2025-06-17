7e9b5bcf8a49a4c2bd175647e34e62d9a75b8677
TimPulin <tigeronsun@gmail.com>
2025-06-17 09:10:18 +0300

    323

---
+++ practice/3/p322.js
@@ -0,0 +1,40 @@
+/*
+Функция updateShoppingCart позволяет увеличить или уменьшить количество какого-то товара в корзине. Она принимает три аргумента:
+
+shoppingCart — объект с текущим набором продуктов (ключ — название продукта, значение — количество, целое положительное число);
+operation — строка с типом операции: "add" или "remove";
+product — название добавляемого товара (это может быть товар из корзины или какой-то новый).
+Вернуть нужно новый объект, в котором количество соответствующего продукта изменено. Исходный объект мутировать нельзя.
+*/
+
+
+const ADD = 'add';
+const REMOVE = 'remove';
+
+const operationValues = {
+  [ADD]: 1,
+  [REMOVE]: -1,
+}
+
+function updateShoppingCart(shoppingCart, operation, product) {
+  const newCart = {...shoppingCart};
+
+  const productAmount = calcProductAmount(newCart, operation, product);
+
+  productAmount <= 0 ? delete newCart[product] : newCart[product] = productAmount;
+
+  return newCart;
+}
+
+function calcProductAmount(cart, operation, product) {
+  const currentAmount = cart[product] ?? 0;
+  return currentAmount + getOperationValues(operation);
+}
+
+
+function getOperationValues(operation) {
+  if (operationValues[operation] === 'undefined') throw new Error(`operation should be ${Object.keys(operationValues).join(', ')}, got ${operation}`);
+  return operationValues[operation];
+}
+
+
---
+++ practice/3/p323.js
@@ -0,0 +1,21 @@
+/*
+Реализуйте функцию, сравнивающую два объекта. Гарантируется, что значениями в этих объектах будут только примитивы.
+*/
+
+function compareObjects(o1, o2) {
+  const o1Keys = Object.keys(o1);
+  const o2Keys = Object.keys(o2);
+
+  if (o1Keys.length !== o2Keys.length) return false;
+
+  for (const key of o1Keys) {
+    if (!Object.hasOwn(o2, key)) return false;
+    if (o1[key] !== o2[key] && !(isNaNEqual(o1[key], o2[key]))) return false;
+  }
+
+  return true;
+}
+
+function isNaNEqual(value1, value2) {
+  return Number.isNaN(value1) && Number.isNaN(value2); 
+}
